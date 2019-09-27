4c35bdf755083bded7b7b571df90be5b412012d4
Da Vinci <@>
2019-09-27 03:48:38 +0300

    zeroize

--- src/hash/mod.rs
+++ src/hash/mod.rs
@@ -12,9 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-
 //! Hash Functions
-
 mod sip;
 pub use sip::SipHasher24 as SipHasher;
 
@@ -25,6 +23,4 @@ pub use types::{
     Hashed,
     DefaultHashable
 };
-
-
 
